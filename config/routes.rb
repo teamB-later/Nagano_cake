ac860d5f0b2989199933ded6dad2ed39c0d75a0e
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-08-16 09:26:24 +0000

    [Add]create_all_controllers homes_route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,51 @@
 Rails.application.routes.draw do
+  root to: 'public/homes#top'
+  get 'about' => 'public/homes#about'
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/confirm'
+    get 'orders/thanks'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/unsubscribe'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+
   # 顧客用
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",

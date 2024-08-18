7335004a07800ed39df91b2f495d222bba6f784f
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-08-18 06:22:49 +0000

    [Add]Bootstrap

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,9 +1,12 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genre = Genre.new
+    @genres = Genre.all
   end
 
   def create
-
+    @genre = Genre.new(genre_params)
+    @genre.save
   end
 
   def edit
@@ -11,5 +14,11 @@ class Admin::GenresController < ApplicationController
 
   def update
 
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).premit(:name)
   end
 end

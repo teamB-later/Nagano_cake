423094b98987743d1cb72726c8b8d02c9134e654
EC2 Default User <ec2-user@ip-172-31-14-221.ap-northeast-1.compute.internal>
2024-08-18 06:30:34 +0000

    [add]customer_index

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,5 +1,6 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def show
@@ -10,5 +11,11 @@ class Admin::CustomersController < ApplicationController
 
   def update
 
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :address, :post_code, :telephone_number, :is_active)
   end
 end

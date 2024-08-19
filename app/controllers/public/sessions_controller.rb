eaec15c5871025ab3e671fdb2e592f9efcc6be46
Ryookkey <173231919+Ryookkey@users.noreply.github.com>
2024-08-19 05:12:39 +0000

    [Add]public/registrations_controller, public/sessions_controller

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,14 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  def after_sign_in_path_for(resource)
+    case resource
+    when admin
+      admin_path
+    when public
+      root_path
+    end
+  end
 
   # GET /resource/sign_in
   # def new

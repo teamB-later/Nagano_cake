47eee1c669aeb85deaabe96611e79e633449244c
EC2 Default User <ec2-user@ip-172-31-14-221.ap-northeast-1.compute.internal>
2024-08-19 13:33:46 +0000

    [add]push

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -18,11 +18,11 @@ class Admin::CustomersController < ApplicationController
       redirect_to admin_customer_path(customer.id)
     else
       render :edit
-    end 
+    end
   end
-  
+
   private
-  
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :address, :post_code, :telephone_number, :is_active)
   end

aa1edaeee30d8eebdcfbcf3da7ad755d61f810eb
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-08-21 09:39:07 +0000

    [Add]save new

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,6 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
   end
 
   def confirm
@@ -9,12 +10,23 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-
+    @order = Order.new(order_params)
+    order_data = order_data_params
   end
 
   def index
   end
 
   def show
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:address, :name, )
+  end
+
+  def order_data_params
+    params[:order][:order_data]
   end
 end

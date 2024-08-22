be05a226704203ac91b97079f2fb8f37be53f023
Ryookkey <173231919+Ryookkey@users.noreply.github.com>
2024-08-22 07:36:53 +0000

    [Add}making_status

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,14 +9,16 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     if @order.update(order_params)
-      redirect_to admin_order_path(@order), notice: '注文ステータスが更新されました。'
+      redirect_to admin_order_path(@order)
     else
       render :show
     end
+    
   end
 
   private
   def order_params
     params.require(:order).permit(:status)
   end
+  
 end

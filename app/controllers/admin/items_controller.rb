6fd56a6ff4a64c8790e96ab5f2eb638ad912ced0
souta2024 <173272685+souta2024@users.noreply.github.com>
2024-08-23 11:52:16 +0000

    [Finish]finish_all

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,7 +5,6 @@ class Admin::ItemsController < ApplicationController
 
   def new
     @item = Item.new
-    @genres = Genre.all
   end
 
   def create
@@ -19,10 +18,13 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
-
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item)
   end
 
   private

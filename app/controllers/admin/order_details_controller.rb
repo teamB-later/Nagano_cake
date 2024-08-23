cef390dcd66b0ab6069b782aef3bc014f904e7e0
Ryookkey <173231919+Ryookkey@users.noreply.github.com>
2024-08-23 09:46:29 +0000

    [add]auto chenging status

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,16 +1,33 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
-    @order = Order.find(params[:id])
     @order_detail = OrderDetail.find(params[:id])
+    @order_id = @order_detail.order_id
+    @order = Order.find(@order_id)
+
+    if @order_detail.making_status == "making"
+      @order.update(status:"making")
+    end
+
     if @order_detail.update(order_detail_making_status_params)
       redirect_to admin_order_path(@order)
     else
       render :show
     end
+
+
+
+
+    # case OrderDetail.making_statuses.key("製作中")
+    #   when 2
+    #     @order.statuses.key = 2
+    # end
+
   end
-  
+
   private
   def order_detail_making_status_params
     params.require(:order_detail).permit(:making_status)
   end
+
+
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,18 +7,26 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
-    @order = Order.find(params[:id])
+     @order = Order.find(params[:id])
+
+    # if OrderDetail.making_statuses.key("製作中")
+    #   @order.statuses.key = "製作中"
+    # end
+    
+    
+
     if @order.update(order_params)
       redirect_to admin_order_path(@order)
     else
       render :show
     end
-    
   end
+
+
 
   private
   def order_params
     params.require(:order).permit(:status)
   end
-  
+
 end
